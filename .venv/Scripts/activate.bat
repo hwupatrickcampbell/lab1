0bf9de09e96a389bd0c38ebc336d3897ca70264a
hwusantiago <s.chumbe@hw.ac.uk>
2023-05-22 15:41:30 +0100

    Add all my files

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sc19\Documents\teaching\2022-2023\F28IR\labs\Python\lab1\.venv
+set VIRTUAL_ENV=C:\Users\sc19\Documents\teaching\2022-2023\F28IR\backups\lab1\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
